825ab2d75489a28c0198f7d9ef49f228083e4a12
l16270175 <86699981+l16270175@users.noreply.github.com>
2021-06-30 06:30:27 -0500

    Delete apps/aplication directory

--- apps/aplication/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-#admin.site.register(Cliente)
-admin.site.register(Producto)
-admin.site.register(Pedido)
-#admin.site.register(PedidoProducto)
